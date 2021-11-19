6692423b338da7ca5ccadcf47e4e8afc5b8bdd8d
tknightz <hatuyen2699@gmail.com>
2021-11-19 17:13:44 +0700

    remove toggle_all() function

--- lua/telescope/_extensions/termfinder/actions.lua
+++ lua/telescope/_extensions/termfinder/actions.lua
@@ -9,7 +9,7 @@ local transform_mod = require('telescope.actions.mt').transform_mod
 local M = {}
 
 local function open_term(prompt_bufnr, direction)
-    local entry = action_state.get_selected_entry(prompt_bufnr)
+    local entry = action_state.get_selected_entry()
     if not entry then
         return
     end
@@ -25,7 +25,7 @@ local function open_term(prompt_bufnr, direction)
 end
 
 M.select_term = function(prompt_bufnr)
-    local entry = action_state.get_selected_entry(prompt_bufnr)
+    local entry = action_state.get_selected_entry()
     if not entry then
         return
     end
@@ -35,13 +35,13 @@ M.select_term = function(prompt_bufnr)
     if term:is_open() then
         vim.api.nvim_set_current_win(term.window)
     else
-        toggleterm.toggle_all("close")
-        term:toggle()
+        -- toggleterm.toggle_all('close')
+        term:open()
     end
 end
 
 M.rename_term = function(prompt_bufnr)
-    local entry = action_state.get_selected_entry(prompt_bufnr)
+    local entry = action_state.get_selected_entry()
     local new_name = vim.fn.input('Rename to: ')
     
     vim.api.nvim_buf_set_name(entry.bufnr, new_name)
@@ -49,7 +49,7 @@ M.rename_term = function(prompt_bufnr)
 end
 
 M.delete_term = function(prompt_bufnr)
-    local term_id = action_state.get_selected_entry(prompt_bufnr).id
+    local term_id = action_state.get_selected_entry().id
     terminal.delete(term_id)
 end
 
