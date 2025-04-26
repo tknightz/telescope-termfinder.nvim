30c805316e14bd28be2883b6bddd7fd5743f47dc
alps2006 <guoxiangxun@gmail.com>
2025-04-26 21:34:35 +0800

    feat: Added term current directory to list for selection.

--- lua/telescope/_extensions/termfinder/util.lua
+++ lua/telescope/_extensions/termfinder/util.lua
@@ -36,4 +36,15 @@ M.get_term_id_by_bufnr = function(bufnr)
     return nil
 end
 
+M.str_split = function(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
 return M
